bbc5e9bb19b62715e8707be64f86d47f3d51cee7
vincent <vincent.chen@efwuhn.com>
2013-12-26 09:22:25 +0800

    Adding boot up script

---
+++ bootup.sh
@@ -0,0 +1,7 @@
+open /Applications/Twitter.app;
+open /Applications/Safari.app;
+open /Applications/Evernote.app;
+open /Applications/Skype.app;
+open /Applications/Xcode.app;
+open /Applications/Mail.app;
+open /Applications/Google\ Chrome.app;
